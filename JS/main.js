bda7d3dc06d7fdc82801f7f28f3af9a4ab5d9e5e
BrisleyF <fajardobrisley@gmail.com>
2022-07-26 12:20:11 -0400

    JS

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+const iconoMenu = document.getElementById('menu-hamburguesa');
+const menu = document.getElementById('menu');
+
+iconoMenu.addEventListener('click', function() {
+    menu.classList.toggle('active');
+    
+});
